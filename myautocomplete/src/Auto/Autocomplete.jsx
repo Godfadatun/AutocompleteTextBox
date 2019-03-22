9bd710577041ac00c3ff1285a102e082c1e98b42
Godfadatun <danyadegoke@gmail.com>
2019-03-22 14:52:00 +0100

    onTextChanged

--- myautocomplete/src/Auto/Autocomplete.jsx
+++ myautocomplete/src/Auto/Autocomplete.jsx
@@ -15,27 +15,53 @@ export default class Autocomplete extends React.Component{
                 'Victor'
             ];
             this.state = {
-                suggestion : []
+                suggestions : []
             }
     }
 
+    // THIS CHECKS IF THERE IS AN INPUT AND IF THERE IT SORTS IT AND FILTERS IT FROM THE SUGGESTION
     onTextChanged = (e) => {
         const value = e.target.value;
-        if(value.lenght === 0){
+        let suggestions = [];
+        if(value.length > 0){
+            const regex = new RegExp(`^${value}`, 'i');
+            suggestions = this.items.sort().filter(v => v.test(regex));
+        }
+        this.setState(() => ({suggestions}))
+
+        //OPTION 2
+        /*
+        if(value.length === 0){
             this.setState(() => ({
                 suggestion: []
-            }))
-        }
+            }));
+        }else {
+            const regex = new RegExp(`^${value}`, 'i');
+            const suggestions = this.items.sort().filter(v => v.test(regex));
+            this.setState(() => ({suggestions}))
+        }*/
+    }
+
+    renderSuggestions(){
+        const { suggestions } = this.state;     //THIS DESTRUCTURES SUGGESTION FROM THE STATE
+        if (suggestions.length === 0) {
+            return null;
+        }return (
+            <ul>
+              {this.suggestions.map(i => <li>{i}</li>)}
+            </ul>
+        )
     }
 
     render(){
         return(
         <div>
             <input onChange={this.onTextChanged} type='text' />
-                <ul>
+            {this.renderSuggestions}
+                {/*<ul> */}
                     {/*were "i" was used tutor used "item" */}
-                    {this.item.map(i => <li>{i}</li>)}
-                </ul>
+                   {/* {this.item.map(i => <li>{i}</li>)} */}
+                {/* </ul> */}
         </div>
     )
     }
